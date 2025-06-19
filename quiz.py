a7aa69ff2945182dc52a77dac5a309184b818411
kakswear5 <137601406+kakswear5@users.noreply.github.com>
2025-06-19 18:58:53 +0300

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,91 @@
+import pgzrun
+
+TITLE = "Quiz_Master"
+
+WIDTH  = 850
+HEIGHT = 650
+
+markquee_box =Rect(40,0,900,80)
+quebox=Rect(50,90,450,120)
+
+
+def draw():
+    screen.clear()
+    screen.draw.filled_rect(markquee_box,"red")
+    screen.draw.filled_rect(quebox,"orange")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pgzrun.go()
